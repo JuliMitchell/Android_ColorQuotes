60035f6a81119e98961031c11f7c3128b42f30e8
Julián Mitchell <julian.mitchell@hotmail.com>
2019-12-14 13:10:08 -0300

    Chaing the id name of the Main activity elements, and instantiating the elements in main activity java class

--- app/src/main/java/com/jmitchell/colorquotes/MainActivity.java
+++ app/src/main/java/com/jmitchell/colorquotes/MainActivity.java
@@ -3,12 +3,23 @@ package com.jmitchell.colorquotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView citaTextView;
+    private TextView autorTextView;
+    private Button nuevaCitaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        citaTextView = findViewById(R.id.citaTextView);
+        autorTextView = findViewById(R.id.autorTextView);
+        nuevaCitaButton = findViewById(R.id.nuevaCitaButton);
+
     }
 }
