d1571a5a2e5f3199d7dbb7a4376b4c1c770499dc
Julián Mitchell <julian.mitchell@hotmail.com>
2019-12-14 13:52:46 -0300

    Adding citas and autores and changing button event to apply citas and autores using a random number

--- app/src/main/java/com/jmitchell/colorquotes/MainActivity.java
+++ app/src/main/java/com/jmitchell/colorquotes/MainActivity.java
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView autorTextView;
     private Button nuevaCitaButton;
 
+    private String autores[];
+    private String citas[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +38,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         */
+
+        autores = new String[5];
+        citas = new String[5];
+
+        autores[0] = "Walt Disney";
+        citas[0] = "Aprendí que lo difícil no es llegar a la cima, sino jamás dejar de subir";
+
+        autores[1] = "Albert Einstein";
+        citas[1] = "La imaginación es más importante que el conocimiento";
+
+
+        autores[2] = "Steve Jobs";
+        citas[2] = "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona";
+
+        autores[3] = "Albert Camus";
+        citas[3] = "El éxito es fácil de obtener. Lo difícil es merecerlo.";
+
+        autores[4] = "Irving Berlin";
+        citas[4] = "El sabio no dice lo que sabe, y el necio no sabe lo que dice.";
+
+
     }
 
     public void clickNuevaCita(View view){
-        Log.d(TAG, "Mensaje enviado desde el evento");
+        int randomNumber = getRandomNumber(autores.length);
+
+        String cita = citas[randomNumber];
+        String autor = autores[randomNumber];
+
+        citaTextView.setText(cita);
+        autorTextView.setText(autor);
+
     }
 
     public int getRandomNumber(int max){
